efdb1b5a8874b79f530f4b468a07952ad21053ab
Ignat Zakrevsky <iezakrevsky@gmail.com>
2022-10-29 21:31:29 +0100

    Fix tests for new MiniTest

--- test/models/arcadia/home_test.rb
+++ test/models/arcadia/home_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class HomeTest < ActiveSupport::TestCase
   test ".index" do
     id = any_id
-    mock = Minitest::Mock.new.expect(:order, [id], [id: :desc])
+    mock = Minitest::Mock.new.expect(:order, [id], [], id: :desc)
 
     assert_equal Arcadia::Home.index(order_class: mock), [id]
   end
